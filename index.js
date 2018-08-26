dc9e319d66c0be34fd105263a22d4806673aa368
david.vega@snhu.edu <david.vega@snhu.edu>
2018-08-26 00:58:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,6 @@ if (string === string.toLowerCase()){
   return 'I can\'t hear you!'
 }
 
-function sayHiToGrandma(string){
-else if (string === string.toUpperCase()){
-  return 'YES, INDEED!'
-}
-
 else if (string === 'I love you, Grandma.'){
   return 'I love you, too.'
 }
